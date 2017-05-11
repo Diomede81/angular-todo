3eef357620c2e8b0480a7776194ce616d417b23a
lucalicata <lucalicata@hotmail.com>
2017-05-11 16:27:06 +0100

    First Version of the app

---
+++ js/app.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Diomede on 09/05/2017.
+ */
+angular.module('ToDoApp', ['ngRoute', 'RouteControllers','UserService','angular-storage','TodoService']);
+
+angular.module('ToDoApp').config(function($locationProvider, $routeProvider){
+
+    $locationProvider.html5Mode(true);
+
+    $routeProvider.when('/',{
+        templateUrl: 'templates/home.html',
+        controller: 'HomeController'
+
+    })
+        .when('/accounts/register', {
+            templateUrl:'templates/register.html',
+            controller:'RegisterController'
+
+        })
+
+        .when('/todo', {
+
+            templateUrl: 'templates/todo.html',
+            controller: 'TodoController'
+    })
+
+        .when('/todo/edit/:id', {
+            templateUrl:'templates/edit-todo.html',
+            controller: 'EditTodoController'
+
+    });
+
+});
---
+++ js/service.js
@@ -0,0 +1,41 @@
+/**
+ * Created by Diomede on 09/05/2017.
+ */
+angular.module('UserService',[])
+        .factory('UserAPIService',function($http){
+
+            UserAPIService = {
+                callAPI: function(url,data){
+                    return $http.post(url,data);
+                }
+            };
+            return UserAPIService;
+        });
+
+angular.module('TodoService',[])
+.factory('TodoAPIService', function($http){
+
+    TodoAPIService = {
+        getTodos: function(url, data,token){
+            var header = "Authorization: JWT " + token;
+            return $http.get(url,{params:{"username":data}},header);
+        },
+        createTodo: function(url, data, token){
+            header = "Authorization: JWT " + token;
+            return $http.post(url,data,header);
+        },
+
+        editTodo: function(url, data, token){
+            header = "Authorization: JWT " + token;
+            return $http.put(url, data,header);
+        },
+
+        deleteTodo: function(url, token){
+            header = "Authorization: JWT " + token;
+            return $http.delete(url, header);
+        }
+
+
+    };
+    return TodoAPIService;
+});
